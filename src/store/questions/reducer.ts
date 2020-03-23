2e0d97cb3fd8caabcb6da12f39850e4696bdc015
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-23 23:04:28 +0100

    add current question to reducer

--- src/store/questions/reducer.ts
+++ src/store/questions/reducer.ts
@@ -1,5 +1,6 @@
 import { Question } from "../../types/question";
 import { AppActions } from "../appActions";
+import { SET_CURRENT_QUESTION } from "./types";
 
 interface QuestionStateType {
   all: Question[];
@@ -13,6 +14,12 @@ const initialState: QuestionStateType = {
 
 export const questionReducer = (state = initialState, action: AppActions) => {
   switch (action.type) {
+    case SET_CURRENT_QUESTION: {
+      return {
+        ...state,
+        current: action.payload
+      };
+    }
     default:
       return state;
   }
