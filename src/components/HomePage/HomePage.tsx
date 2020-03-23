481cdd9af40cbc46a4f1041863ce8683382606d6
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-23 11:51:08 +0100

    first commit

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type HomePageProps = {};
+
+const HomePage: React.FC<HomePageProps> = () => {
+  return (
+    <div>
+      <h1>Welcome to Should-I-Go-Out?</h1>
+    </div>
+  );
+};
+
+export default HomePage;
