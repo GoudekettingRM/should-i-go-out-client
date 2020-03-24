097be1516ebb3755a0a6e3cd79dca43471c1c97d
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-24 14:13:47 +0100

    add react-bootstrap

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -9,30 +9,6 @@ declare global {
   }
 }
 
-// ------------------------------USE THIS FOR REDUX PERSIST ----------------------//
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   whitelist: ["session"]
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-//   ? window.__REDUX_DEVTOOLS_EXTENSION__()
-//   : x => x;
-
-// const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
-
-// const store = createStore(persistedReducer, enhancer);
-// const persistor = persistStore(store);
-
-// export { store, persistor };
-
-// ------------------------------- USE THIS FOR STORE WITHOUT PERSIST ---------------------//
-
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
   ? window.__REDUX_DEVTOOLS_EXTENSION__()
   : (x: any) => x;
