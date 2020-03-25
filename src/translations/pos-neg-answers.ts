79ba5649a5688493e09661f40f2febd80e922f1f
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-25 21:07:40 +0100

    add german

--- src/translations/pos-neg-answers.ts
+++ src/translations/pos-neg-answers.ts
@@ -8,15 +8,18 @@ export const positiveAnswers: PositiveNegativeAnswers = [
   "yes, by myself or with people I live with",
   "ja, alleen of alleen met mensen die bij je in huis wonen",
   "si, solo o con las personas que vivo",
-  "sim, sozinho ou com pessoas com quem vivo"
+  "sim, sozinho ou com pessoas com quem vivo",
+  "ja, alleine oder mit Leuten mit denen ich zusammenwohne"
 ];
 
 export const negativeAnswers: string[] = [
   "no",
   "nee",
   "não",
+  "nein",
   "no, with others",
   "nee, ook met andere mensen",
   "no, con mas gente",
-  "não, vou sair com outras pessoas"
+  "não, vou sair com outras pessoas",
+  "nein, mit anderen Leuten"
 ];
